647c3818191099e5f389f3cf0260ba58e6dbb0cc
Yen Tran <yentran@Yens-MBP.lan>
2025-01-11 15:54:26 -1000

    adds current word section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Status from './components/Status'
 import Languages from './components/Languages'
 
 export default function AssemblyEndgame() {
-    return (
-        <main>
-          <Header />
-          <Status />
-          <Languages />
-        </main>
-    )
+  const [currentWord, setCurrentWord] = useState('react');
+
+  const wordElements = currentWord.split('').map((letter, index) => <span
+    className='word-letter'
+    key={index}>{letter.toUpperCase()}</span>)
+
+  return (
+      <main>
+        <Header />
+        <Status />
+        <Languages />
+        <section className='word-container'>
+          {wordElements}
+        </section>
+      </main>
+  )
 }
